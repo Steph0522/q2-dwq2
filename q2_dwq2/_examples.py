07a241229c9adf88ca8657b523c1999df7d1f5a2
Steph0522 <shereirap@gmail.com>
2025-01-23 19:01:26 -0600

    Add pipeline

--- q2_dwq2/_examples.py
+++ q2_dwq2/_examples.py
@@ -9,18 +9,14 @@ from q2_dwq2 import SingleRecordDNAFASTAFormat
 
 def seq1_factory():
     seq = skbio.DNA("AACCGGTTGGCCAA", metadata={"id": "seq1"})
-    return _create_seq_artifact(seq)
+    return qiime2.Artifact.import_data(
+        "SingleDNASequence", seq, view_type=skbio.DNA)
 
 
 def seq2_factory():
     seq = skbio.DNA("AACCGCTGGCGAA", metadata={"id": "seq2"})
-    return _create_seq_artifact(seq)
-
-
-def _create_seq_artifact(seq: skbio.DNA):
-    ff = SingleRecordDNAFASTAFormat()
-    seq.write(str(ff.path))
-    return qiime2.Artifact.import_data("SingleDNASequence", ff)
+    return qiime2.Artifact.import_data(
+        "SingleDNASequence", seq, view_type=skbio.DNA)
 
 def nw_align_example_1(use):
     seq1 = use.init_artifact('seq1', seq1_factory)
@@ -32,3 +28,16 @@ def nw_align_example_1(use):
         use.UsageInputs(seq1=seq1, seq2=seq2),
         use.UsageOutputNames(aligned_sequences='msa'),
     )
+def align_and_summarize_example_1(use):
+    seq1 = use.init_artifact('seq1', seq1_factory)
+    seq2 = use.init_artifact('seq2', seq2_factory)
+
+    msa, msa_summary, = use.action(
+        use.UsageAction(plugin_id='dwq2',
+                        action_id='align_and_summarize'),
+        use.UsageInputs(seq1=seq1, seq2=seq2),
+        use.UsageOutputNames(aligned_sequences='msa',
+                             msa_summary='msa_summary'),
+    )
+
+
--- q2_dwq2/tests/test_transformers.py
+++ q2_dwq2/tests/test_transformers.py
@@ -8,20 +8,21 @@ from q2_dwq2 import SingleRecordDNAFASTAFormat
 class SingleDNASequenceTransformerTests(TestPluginBase):
     package = 'q2_dwq2.tests'
 
-    def test_single_record_fasta_to_DNA_simple1(self):
-        _, observed = self.transform_format(
-            SingleRecordDNAFASTAFormat, DNA, filename='seq-1.fasta')
+    def test_DNA_to_single_record_fasta_simple1(self):
+        in_ = DNA('ACCGGTGGAACCGGTAACACCCAC',
+                  metadata={'id': 'example-sequence-1', 'description': ''})
+        tx = self.get_transformer(DNA, SingleRecordDNAFASTAFormat)
 
-        expected = DNA('ACCGGTGGAACCGGTAACACCCAC',
-                       metadata={'id': 'example-sequence-1', 'description': ''})
+        observed = tx(in_)
+        # confirm "round-trip" of DNA -> SingleRecordDNAFASTAFormat -> DNA
+        # results in an observed sequence that is the same as the starting
+        # sequence
+        self.assertEqual(observed.view(DNA), in_)
 
-        self.assertEqual(observed, expected)
+    def test_DNA_to_single_record_fasta_simple2(self):
+        in_ = DNA('ACCGGTAACCGGTTAACACCCAC',
+                  metadata={'id': 'example-sequence-2', 'description': ''})
+        tx = self.get_transformer(DNA, SingleRecordDNAFASTAFormat)
 
-    def test_single_record_fasta_to_DNA_simple2(self):
-        _, observed = self.transform_format(
-            SingleRecordDNAFASTAFormat, DNA, filename='seq-2.fasta')
-
-        expected = DNA('ACCGGTAACCGGTTAACACCCAC',
-                       metadata={'id': 'example-sequence-2', 'description': ''})
-
-        self.assertEqual(observed, expected)
+        observed = tx(in_)
+        self.assertEqual(observed.view(DNA), in_)
